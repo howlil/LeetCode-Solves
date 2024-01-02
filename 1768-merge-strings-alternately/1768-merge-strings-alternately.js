13d5bc9483ebc145e8fc87fd27f6c5f5153efe5e
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2024-01-02 20:09:59 +0700

    Time: 52 ms (60.50%), Space: 41.9 MB (84.28%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let str1 = 0;
+    let str2 = 0;
+    let len = word1.length > word2.length ? word1.length : word2.length;
+    let result = '';
+    for (let i = 0; i < len; i++) {
+        result += (word1[i] ? word1[i] : '')+(word2[i] ? word2[i] : '');
+    }
+    return result;
+};
