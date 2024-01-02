5e8a674d82f2c0c3c6ed9bd6ae7723051453d14e
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2024-01-02 17:33:07 +0700

    update

--- 30DaysJS/Function Transformations/FunctionComposition.js
+++
@@ -1,9 +0,0 @@
-var compose = function(functions) {
-    return function(x) {
-        let currentNum = x;
-        functions.reverse().forEach((fn) => {
-            currentNum = fn(currentNum);
-        });
-        return currentNum;
-    };
-};  
