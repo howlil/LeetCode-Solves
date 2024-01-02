03359e0160edcf90084764d14d9e68bc8445ec29
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2024-01-02 20:19:02 +0700

    Time: 107 ms (5.07%), Space: 42 MB (33.44%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let filterArr = [];
+    arr.forEach((e,i) => {
+        if(fn(e,i)) {
+            filterArr.push(e);
+        }
+    });
+    return filterArr;
+};
